f9467a9be5662ad77c1799de463caf45d0e9a15f
Guilherme <guilherme.cunhaescobar@gmail.com>
2026-09-10 15:44:20 -0300

    Adiciona verificações de estoque ao sistema de adição de produtos no carrinho

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -13,33 +13,53 @@ const formatZodErrors = require("../utils/formatZodErrors")
 
 //Controllers
 
-async function addProduct(req, res) { //Falta integrar com controle de estoque !
+async function addProduct(req, res) { 
     const {...data} = req.body
 
-    const user = req.user
     const product = await Product.findById(data.productId)
 
     if(!product){
         addFlash(req, 'alert-danger' ,'Produto nao encontrado, tente novamente mais tarde')
         return res.redirect("/products")
     }
-
+    //Procura varição correspondente ao que o usuario escolheu
     const variation = product.variations.find(variation => 
         variation.color === data.color && 
         variation.size === data.size
     )
-
+    //verifica se a variação existe
     if(!variation){
         addFlash(req, 'alert-danger', 'Produto nao encontrado, tente novamente mais tarde')
         return res.redirect('/products')
     }
 
+    //procura se o usuario ja tem uma variação igual no carrinho
+    const cartItem = req.user.cart.find(item => 
+        item.variationId.toString() === variation._id.toString()
+    )
+
+    //Se tiver variação igual no carrinho a variavel pega, se nao, vira 0
+    const currentQuantity = cartItem ? cartItem.quantity : 0
+
+    if(variation.stock < Number(data.quantity) + currentQuantity ){
+        addFlash(req, "alert-danger", 'Não há estoque disponivel para a sua demanda, Tente novamente mais tarde')
+        console.log('Quantidade indisponivel pros guri')
+        return res.redirect('/products')
+    }
+
+    if(!cartItem){
+
     req.user.cart.push({
         productId: product._id,
         variationId: variation._id,
-        quantity: data.quantity
+        quantity: Number(data.quantity)
         
     })
+    }
+
+    else{
+        cartItem.quantity += Number(data.quantity)
+    }
 
     await req.user.save()
     addFlash(req, "alert-success", "Produto Adicionado Ao Carrinho !")
--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const cartController = require("../controllers/cartController")
 const { isAuthenticated } = require('../middlewares/auth')
 
-
+router.use(isAuthenticated)
 
 router.post('/add', cartController.addProduct)
 
